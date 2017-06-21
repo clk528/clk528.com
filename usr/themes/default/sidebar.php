52b6ec4880972a3a3a9925fa29a013e8b60d035f
逝去 <clk@528.qq.com>
2017-06-21 23:57:23 +0800

    编辑sidebar

--- usr/themes/default/sidebar.php
+++ usr/themes/default/sidebar.php
@@ -41,7 +41,7 @@
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowOther', $this->options->sidebarBlock)): ?>
 	<section class="widget">
-		<h3 class="widget-title"><?php _e('其它'); ?></h3>
+		<!-- <h3 class="widget-title"><?php _e('其它'); ?></h3> -->
         <ul class="widget-list">
             <?php if($this->user->hasLogin()): ?>
 				<li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
@@ -49,9 +49,9 @@
             <?php else: ?>
                 <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li>
             <?php endif; ?>
-            <li><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章 RSS'); ?></a></li>
-            <li><a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论 RSS'); ?></a></li>
-            <li><a href="http://www.typecho.org">Typecho</a></li>
+            <!-- <li><a href="<?php /*$this->options->feedUrl();*/ ?>"><?php _e('文章 RSS'); ?></a></li>
+            <li><a href="<?php /*$this->options->commentsFeedUrl(); */?>"><?php _e('评论 RSS'); ?></a></li>
+            <li><a href="http://www.typecho.org">Typecho</a></li> -->
         </ul>
 	</section>
     <?php endif; ?>
